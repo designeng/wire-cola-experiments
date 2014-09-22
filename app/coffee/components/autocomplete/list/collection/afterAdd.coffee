f11917d23a2612e5ddcdbeeeee9ad237631b8745
Denis Savenok <savenok@agent.ru>
2014-09-22 21:19:53 +0400

    removed components; cleaned code

--- app/coffee/components/autocomplete/list/collection/afterAdd.coffee
+++
@@ -1,7 +0,0 @@
-define ["when"], (When) ->
-    
-    afterAdd = (promise) ->
-        console.log  "___afterAdd", @, promise
-
-        When(promise).then (item) ->
-            console.log "__ITEM", item
--- app/js/components/underscore/viewSource.js
+++
@@ -1,8 +0,0 @@
-define(["underscore", "when"], function(_, When) {
-  var viewSource;
-  return viewSource = function(template, model) {
-    var html;
-    html = _.template(template, model);
-    return html;
-  };
-});
